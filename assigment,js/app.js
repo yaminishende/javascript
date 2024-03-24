857557e884bc87e6153bc7f3e87008d7b66cdef8
yaminishende <140809296+yaminishende@users.noreply.github.com>
2024-03-24 11:53:20 -0700

    Add files via upload

---
+++ assigment,js/app.js
@@ -0,0 +1,82 @@
+//
+//let b=10; 1)Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+//let  b=13
+//let e = 58
+//console.log(b+e);
+//console.log(b*e);
+//console.log(b/e);
+//console.log(b%e);
+//console.log(b-e);
+
+// 2) Take two numbers and only print the integer part when a is divided by b.
+//let q=76;
+//let b=54;
+//console.log(a%b);
+
+//3) Take two numbers a,b and print the exponential power of the first number raised by the second.
+//let  k=2;
+//console.log(k**b);
+
+//4) Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+ //let a=20;
+ //let b=4;
+ //console.log(a%b);
+
+ //5) Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+ //let math =80;
+// let phy =60;
+ //let m=30;
+ //let h=50;
+ //let c=60;
+// console.log(math+phy+m+h+c/5);
+
+//6) Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+//let costp=30;
+//let sellingp=50;
+//console.log(sellingp - costp);
+
+//7) Take a number and print out the last digit of the number.
+
+//let n = 24;
+//console.log(n%10);
+
+//8) Take a 2 digit number and return the first and last digits.
+//let n = 38;
+//console.log (Math.floor(n/10));
+//console.log(n%10);
+
+//9) Write a JavaScript program to find the largest of three numbers.
+//let number1 = 5;
+//let number2 = 30;
+//let number3 = 20;
+//let highnumber = number1 ;
+//if  (number2 > highnumber) {
+   // highnumber = number2;
+
+//}
+ //if (number3 > highnumber) {
+ 
+  //  highnumber = number3;{
+ 
+  //console.log(highnumber);
+
+  //10) Write a JavaScript program to check whether a number is even or odd.
+   //let n =5;
+  // if (n%2==0) {
+    //   console.log("even");{ 
+  // else {
+    //  console.log("odd");}
+   
+
+
+
+
+
+
+
+
+  
+
+
+
+
